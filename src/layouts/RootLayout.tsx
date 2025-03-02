961cd8ecba3499138fee1f745c57544e62bde64b
PedroHRFerreira <devpedrohr@gmail.com>
2025-03-02 16:38:08 -0300

    style: :art: Ajustes no style da page

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,7 +1,5 @@
 import "@/assets/globals.scss";
 import style from "./styles.module.scss";
-import { useState } from "react";
-import AtomsIconSvg from "@/components/atoms/IconSvg/index";
 import OrganismsAsideMenu from "@/components/organisms/AsideMenu/Index";
 
 interface RootLayoutProps {
@@ -9,19 +7,9 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const [isOpen, setIsOpen] = useState(true);
-
   return (
     <div className={style.container}>
-      <aside className={`${style.aside} ${isOpen ? style.open : style.closed}`}>
-        <span className={style.toggleArrow} onClick={() => setIsOpen(!isOpen)}>
-          <AtomsIconSvg
-            className={isOpen ? style.iconOpen : style.iconClosed}
-            width="32px"
-            height="32px"
-            name="seta-para-a-esquerda"
-          />
-        </span>
+      <aside className={`${style.aside}`}>
         <OrganismsAsideMenu />
       </aside>
       <main className={style.main}>{children}</main>
